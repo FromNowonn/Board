321f81402923a7d8f43c41b43472ea4e8e4195c0
jjeong <psmb2006@naver.com>
2021-06-16 16:08:50 +0900

    페이징 처리를 위한 Service 계층 소스 추가

--- src/main/java/Demo/CRUD/Board/service/BoardService.java
+++ src/main/java/Demo/CRUD/Board/service/BoardService.java
@@ -1,6 +1,8 @@
 package Demo.CRUD.Board.service;
 
 import Demo.CRUD.Board.dto.BoardDto;
+import Demo.CRUD.Board.dto.PageRequestDto;
+import Demo.CRUD.Board.dto.PageResultDto;
 import Demo.CRUD.Board.entity.Board;
 
 public interface BoardService {
@@ -14,4 +16,17 @@ public interface BoardService {
                 .build();
         return entity;
     }
+    default BoardDto entityToDto(Board entity){
+        BoardDto dto = BoardDto.builder()
+                .boardId(entity.getBoardId())
+                .boardTitle(entity.getBoardTitle())
+                .boardWriter(entity.getBoardWriter())
+                .boardContents(entity.getBoardContents())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
+    }
+
+    PageResultDto<BoardDto, Board> getList(PageRequestDto pageRequestDto);
 }
--- src/main/java/Demo/CRUD/Board/service/BoardServiceImpl.java
+++ src/main/java/Demo/CRUD/Board/service/BoardServiceImpl.java
@@ -1,15 +1,24 @@
 package Demo.CRUD.Board.service;
 
 import Demo.CRUD.Board.dto.BoardDto;
+import Demo.CRUD.Board.dto.PageRequestDto;
+import Demo.CRUD.Board.dto.PageResultDto;
 import Demo.CRUD.Board.entity.Board;
 import Demo.CRUD.Board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.hibernate.annotations.Target;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
+
 public class BoardServiceImpl implements BoardService{
     private final BoardRepository repository;
     @Override
@@ -21,5 +30,13 @@ public class BoardServiceImpl implements BoardService{
         log.info(entity);
         repository.save(entity);
         return entity.getBoardId();
+    }
+
+    @Override
+    public PageResultDto<BoardDto, Board> getList(PageRequestDto pageRequestDto){
+        Pageable pageable = pageRequestDto.getPageable(Sort.by("boardId").descending());
+        Page<Board> result = repository.findAll(pageable);
+        Function<Board, BoardDto> fn = (entity -> entityToDto(entity));
+        return new PageResultDto<>(result,fn);
     }
 }
