10e952dfcef113b8ffc035860a9b4efd8b8c4ad6
jjeong <psmb2006@naver.com>
2021-06-26 19:34:05 +0900

    test code stop

--- src/test/java/Demo/CRUD/Board/service/BoardServiceTest.java
+++ src/test/java/Demo/CRUD/Board/service/BoardServiceTest.java
@@ -1,40 +1,40 @@
-package Demo.CRUD.Board.service;
-
-import Demo.CRUD.Board.dto.BoardDto;
-import Demo.CRUD.Board.dto.PageRequestDto;
-import Demo.CRUD.Board.dto.PageResultDto;
-import Demo.CRUD.Board.entity.Board;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class BoardServiceTest {
-    @Autowired
-    private BoardService service;
-    @Test
-    public void testRegister(){
-        BoardDto boardDto = BoardDto.builder()
-                .boardTitle("Sample Title..")
-                .boardContents("Sample contents")
-                .boardWriter("Sample writer")
-                .build();
-        System.out.println(service.register(boardDto));
-    }
-    @Test
-    public void testList(){
-        PageRequestDto pageRequestDto = PageRequestDto.builder().page(1).size(10).build();
-        PageResultDto<BoardDto, Board> resultDto = service.getList(pageRequestDto);
-
-        System.out.println("PRE : "+resultDto.isPrev());
-        System.out.println("NEXT : "+resultDto.isNext());
-        System.out.println("TOTAL : "+resultDto.getTotalPage());
-        System.out.println("==============================");
-        for(BoardDto boardDto : resultDto.getDtoList()){
-            System.out.println(boardDto);
-        }
-        System.out.println("==============================");
-        resultDto.getPageList().forEach(i->System.out.println(i));
-    }
-
-}
+//package Demo.CRUD.Board.service;
+//
+//import Demo.CRUD.Board.dto.BoardDto;
+//import Demo.CRUD.Board.dto.PageRequestDto;
+//import Demo.CRUD.Board.dto.PageResultDto;
+//import Demo.CRUD.Board.entity.Board;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class BoardServiceTest {
+//    @Autowired
+//    private BoardService service;
+//    @Test
+//    public void testRegister(){
+//        BoardDto boardDto = BoardDto.builder()
+//                .boardTitle("Sample Title..")
+//                .boardContents("Sample contents")
+//                .boardWriter("Sample writer")
+//                .build();
+//        System.out.println(service.register(boardDto));
+//    }
+//    @Test
+//    public void testList(){
+//        PageRequestDto pageRequestDto = PageRequestDto.builder().page(1).size(10).build();
+//        PageResultDto<BoardDto, Board> resultDto = service.getList(pageRequestDto);
+//
+//        System.out.println("PRE : "+resultDto.isPrev());
+//        System.out.println("NEXT : "+resultDto.isNext());
+//        System.out.println("TOTAL : "+resultDto.getTotalPage());
+//        System.out.println("==============================");
+//        for(BoardDto boardDto : resultDto.getDtoList()){
+//            System.out.println(boardDto);
+//        }
+//        System.out.println("==============================");
+//        resultDto.getPageList().forEach(i->System.out.println(i));
+//    }
+//
+//}
