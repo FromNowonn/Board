203aab286441d334822b8752bc247699d97385ca
jjeong <psmb2006@naver.com>
2021-06-29 19:18:11 +0900

    ec2

--- src/main/java/Demo/CRUD/Board/controller/BoardController.java
+++ src/main/java/Demo/CRUD/Board/controller/BoardController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("UserBoard")
+@RequestMapping("/UserBoard")
 @RequiredArgsConstructor
 @Log4j2
 
