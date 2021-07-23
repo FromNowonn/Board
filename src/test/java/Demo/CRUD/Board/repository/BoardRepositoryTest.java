344aad7136cdef2a96dcb33730d0c0a1f1f5b040
jjeong <psmb2006@naver.com>
2021-07-23 18:47:17 +0900

    test delete

--- src/test/java/Demo/CRUD/Board/repository/BoardRepositoryTest.java
+++ src/test/java/Demo/CRUD/Board/repository/BoardRepositoryTest.java
@@ -20,63 +20,63 @@ import java.util.stream.IntStream;
 @SpringBootTest
 public class BoardRepositoryTest {
 
-    @Autowired
-    private BoardRepository boardRepository;
-
-    @Test
-    public void insertTest(){
-        IntStream.rangeClosed(1, 100).forEach(i->{
-            Board board = Board.builder()
-                    .boardTitle("Title test"+i)
-                    .boardContents("Content test"+i)
-                    .boardWriter("Writer test"+i)
-                    .build();
-            System.out.println(boardRepository.save(board));
-        });
-    }
-
-    @Test
-    public void updateTest(){
-        Optional<Board> result = boardRepository.findById(30L);
-        if(result.isPresent()){
-            Board board = result.get();
-            board.changeTitle("change hello");
-            board.changeContent("change gg");
-            boardRepository.save(board);
-        }
-    }
-
-
-    @Test
-    public void testQuery1(){
-        Pageable pageable = PageRequest.of(0,10, Sort.by("boardId").descending());
-        QBoard qBoard = QBoard.board;
-
-        String keyword = "1";
-        BooleanBuilder builder = new BooleanBuilder();
-        BooleanExpression expression = qBoard.boardTitle.contains(keyword);
-        builder.and(expression);
-        Page<Board> result = boardRepository.findAll(builder,pageable);
-        result.stream().forEach(board -> {
-            System.out.println(board);
-        });
-    }
-    @Test
-    public void testQuery2(){
-        Pageable pageable = PageRequest.of(0,10, Sort.by("boardId").descending());
-        QBoard qBoard = QBoard.board;
-
-        String keyword = "1";
-        BooleanBuilder builder = new BooleanBuilder();
-        BooleanExpression exTitle = qBoard.boardTitle.contains(keyword);
-        BooleanExpression exContents = qBoard.boardTitle.contains(keyword);
-        BooleanExpression exAll = exTitle.or(exContents);
-        builder.and(exAll);
-        builder.and(qBoard.boardId.gt(0L));
-        Page<Board> result = boardRepository.findAll(builder,pageable);
-        result.stream().forEach(board -> {
-            System.out.println(board);
-        });
-    }
+//    @Autowired
+//    private BoardRepository boardRepository;
+//
+//    @Test
+//    public void insertTest(){
+//        IntStream.rangeClosed(1, 100).forEach(i->{
+//            Board board = Board.builder()
+//                    .boardTitle("Title test"+i)
+//                    .boardContents("Content test"+i)
+//                    .boardWriter("Writer test"+i)
+//                    .build();
+//            System.out.println(boardRepository.save(board));
+//        });
+//    }
+//
+//    @Test
+//    public void updateTest(){
+//        Optional<Board> result = boardRepository.findById(30L);
+//        if(result.isPresent()){
+//            Board board = result.get();
+//            board.changeTitle("change hello");
+//            board.changeContent("change gg");
+//            boardRepository.save(board);
+//        }
+//    }
+//
+//
+//    @Test
+//    public void testQuery1(){
+//        Pageable pageable = PageRequest.of(0,10, Sort.by("boardId").descending());
+//        QBoard qBoard = QBoard.board;
+//
+//        String keyword = "1";
+//        BooleanBuilder builder = new BooleanBuilder();
+//        BooleanExpression expression = qBoard.boardTitle.contains(keyword);
+//        builder.and(expression);
+//        Page<Board> result = boardRepository.findAll(builder,pageable);
+//        result.stream().forEach(board -> {
+//            System.out.println(board);
+//        });
+//    }
+//    @Test
+//    public void testQuery2(){
+//        Pageable pageable = PageRequest.of(0,10, Sort.by("boardId").descending());
+//        QBoard qBoard = QBoard.board;
+//
+//        String keyword = "1";
+//        BooleanBuilder builder = new BooleanBuilder();
+//        BooleanExpression exTitle = qBoard.boardTitle.contains(keyword);
+//        BooleanExpression exContents = qBoard.boardTitle.contains(keyword);
+//        BooleanExpression exAll = exTitle.or(exContents);
+//        builder.and(exAll);
+//        builder.and(qBoard.boardId.gt(0L));
+//        Page<Board> result = boardRepository.findAll(builder,pageable);
+//        result.stream().forEach(board -> {
+//            System.out.println(board);
+//        });
+//    }
 
 }
--- src/test/java/Demo/CRUD/Board/service/BoardServiceTest.java
+++ src/test/java/Demo/CRUD/Board/service/BoardServiceTest.java
@@ -12,29 +12,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class BoardServiceTest {
     @Autowired
     private BoardService service;
-    @Test
-    public void testRegister(){
-        BoardDto boardDto = BoardDto.builder()
-                .boardTitle("Sample Title..")
-                .boardContents("Sample contents")
-                .boardWriter("Sample writer")
-                .build();
-        System.out.println(service.register(boardDto));
-    }
-    @Test
-    public void testList(){
-        PageRequestDto pageRequestDto = PageRequestDto.builder().page(1).size(10).build();
-        PageResultDto<BoardDto, Board> resultDto = service.getList(pageRequestDto);
-
-        System.out.println("PRE : "+resultDto.isPrev());
-        System.out.println("NEXT : "+resultDto.isNext());
-        System.out.println("TOTAL : "+resultDto.getTotalPage());
-        System.out.println("==============================");
-        for(BoardDto boardDto : resultDto.getDtoList()){
-            System.out.println(boardDto);
-        }
-        System.out.println("==============================");
-        resultDto.getPageList().forEach(i->System.out.println(i));
-    }
+//    @Test
+//    public void testRegister(){
+//        BoardDto boardDto = BoardDto.builder()
+//                .boardTitle("Sample Title..")
+//                .boardContents("Sample contents")
+//                .boardWriter("Sample writer")
+//                .build();
+//        System.out.println(service.register(boardDto));
+//    }
+//    @Test
+//    public void testList(){
+//        PageRequestDto pageRequestDto = PageRequestDto.builder().page(1).size(10).build();
+//        PageResultDto<BoardDto, Board> resultDto = service.getList(pageRequestDto);
+//
+//        System.out.println("PRE : "+resultDto.isPrev());
+//        System.out.println("NEXT : "+resultDto.isNext());
+//        System.out.println("TOTAL : "+resultDto.getTotalPage());
+//        System.out.println("==============================");
+//        for(BoardDto boardDto : resultDto.getDtoList()){
+//            System.out.println(boardDto);
+//        }
+//        System.out.println("==============================");
+//        resultDto.getPageList().forEach(i->System.out.println(i));
+//    }
 
 }
